259bdcad79f05464bb55d223624b8435b4765925
Ivan Kozhevnikov <ivan.keynes@bbconsult.co.uk>
2021-09-03 18:25:42 +0700

    SPA

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -1,4 +1,4 @@
-import { Route } from "@angular/router";
+import { Routes } from "@angular/router";
 
 import { MenuComponent } from '../menu/menu.component';
 import { DishdetailComponent } from '../dishdetail/dishdetail.component';
@@ -9,5 +9,6 @@ import { ContactComponent } from '../contact/contact.component';
 export const routes:Routes =[
   {path:'home', component: HomeComponent},
   {path:'menu', component: MenuComponent},
+  {path:'contactus', component: ContactComponent},
   {path:'',redirectTo:'/home',pathMatch  :'full'}
 ]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Dish} from '../shared/dish';
+import { DishService } from '../services/dish.service';
+import { Promotion } from '../shared/promotion';
+import { PromotionService } from '../services/dish.service';
 
 @Component({
   selector: 'app-home',
