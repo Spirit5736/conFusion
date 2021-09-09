4c4ee23a0ac66de6e61c69bd69f72dd374287c2d
Ivan Kozhevnikov <ivan.keynes@bbconsult.co.uk>
2021-09-09 14:17:16 +0700

    216090

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -4,6 +4,8 @@ import { Component, OnInit} from "@angular/core";
 import { Params, ActivatedRoute } from "@angular/router";
 import { Location } from "@angular/common";
 import { Dish } from "../shared/dish";
+import { DISHES } from '../shared/dishes';
+import { InitialStylingFlags } from '@angular/core/src/render3/interfaces/definition';
 
 @Component({
   selector: "app-dishdetail",
@@ -20,7 +22,8 @@ export class DishdetailComponent implements OnInit {
 
   ngOnInit() {
     let id = this.route.snapshot.params['id'];
-    this.dish = this.DishService.getDish(id);
+    this.DishService.getDish(id)
+    .then(dish => this.dish = dish);
   }
   goBack(): void{
     this.location.back();
